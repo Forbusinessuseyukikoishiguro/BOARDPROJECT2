652697f1f19727261004c293d83da6bdd9721cb2
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-18 17:39:10 +0900

    0318_1738_画像ファイル設定

--- boardproject/boardproject/urls.py
+++ boardproject/boardproject/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  # include関数をインポート追加
 
+
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boardapp.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
