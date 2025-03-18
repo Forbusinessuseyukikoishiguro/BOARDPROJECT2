7a8fb3c24355a3c0924cd3b3c931aa5621a69df5
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-18 16:49:12 +0900

    0318_1648_管理画面生成

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.db import IntegrityError  # この行を追加
+from django.db import IntegrityError
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
+from .models import BoardModel  # ドットの後にスペースを削除
+
 
 # Create your views here.
 
@@ -13,23 +15,26 @@ def signupfunc(request):
         
         try:
             user = User.objects.create_user(username, '', password)
+            return redirect('login')  # サインアップ成功後にログインページへリダイレクト
         except IntegrityError:
-            return render(request, 'login.html', {'error': 'このユーザーは既に登録されています！'})
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています！'})
     return render(request, 'signup.html', {'some': 100})
+
 
 def loginfunc(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST.get('username')  # POSTの値を取得するメソッドを統一
+        password = request.POST.get('password')  # POSTの値を取得するメソッドを統一
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request, 'login.html', {'context': 'logged in'})
-            ...
+            return redirect('list')  # ログイン成功後にリストページへリダイレクト
         else:
-            return render(request, 'login.html', {'context': 'not logged in'})
+            return render(request, 'login.html', {'error': 'ユーザー名またはパスワードが正しくありません'})
     
-    return render(request, 'login.html', {'context': 'get method'}) 
+    return render(request, 'login.html')
+
 
 def listfunc(request):
-    return render(request,'list.html',{})
+    object_list = BoardModel.objects.all()  # 'object' から 'objects' に修正（重要）
+    return render(request, 'list.html', {'object_list': object_list})
