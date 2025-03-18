7a4bf9d84bf7b55bdfeec29ea10cd2900ffccce9
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-18 16:14:41 +0900

    0318_1614_BoardModel作成

--- boardproject/boardapp/models.py
+++ boardproject/boardapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BoardModel(models.Model):  # models.Modelを継承する必要があります
+    title = models.CharField(max_length=100)  # 'charField'ではなく'CharField'が正しい
+    content = models.TextField()
+    author = models.CharField(max_length=100)  # 'charField'ではなく'CharField'が正しい
+    snsimage = models.ImageField(upload_to='')
+    good = models.IntegerField()  # デフォルト値を設定すると便利
+    read = models.IntegerField()  # デフォルト値を設定すると便利
+    readtext = models.TextField()  # 空を許可する場合はblank=Trueを指定
