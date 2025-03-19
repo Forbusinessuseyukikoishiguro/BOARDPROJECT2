468568f58bd1e69a6544a1f0ff43e88723444dea
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-19 19:17:55 +0900

    0319_1917 Create

--- boardproject/boardapp/models.py
+++ boardproject/boardapp/models.py
@@ -7,6 +7,6 @@ class BoardModel(models.Model):  # models.Modelを継承する必要がありま
     content = models.TextField()
     author = models.CharField(max_length=100)  # 'charField'ではなく'CharField'が正しい
     snsimage = models.ImageField(upload_to='')
-    good = models.IntegerField(default=0)  # このフィールドが必要
-    read = models.IntegerField()  # デフォルト値を設定すると便利
-    readtext = models.TextField()  # 空を許可する場合はblank=Trueを指定
+    good = models.IntegerField(default=0, null=True,blank=True)  # このフィールドが必要
+    read = models.IntegerField(null=True,blank=True)  # デフォルト値を設定すると便利
+    readtext = models.TextField( null=True,blank=True)  # 空を許可する場合はblank=Trueを指定
